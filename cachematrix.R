9fe0e2bd90aa3d5120deec4d87e1db799b1c4ebd
Paul Martin <paul@valravncapital.com>
2018-10-04 16:09:11 -0700

    Add files via upload
    cachmatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## https://github.com/rdpeng/ProgrammingAssignment2
 
-## Write a short comment describing this function
+## This assignment is to write two functions that:
+## (1) create a special matrix, and 
+## (2) cache the inverse of the special matrix
+
+## makeCacheMatrix is a function that creates a special 
+## "matrix" object that cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## CacheSolve is a function that computes the inverse of
+## the special "matrix" returned by the MakeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data,...)
+    x$setinv(inv)
+    inv
 }
+
+## Testing the functions
+testMatrix1 <- matrix(rnorm(16),4,4)
+testMatrix1
+testMatrix2 <- makeCacheMatrix(testMatrix1)
+cacheSolve(testMatrix2)
